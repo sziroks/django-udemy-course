4971861e73e23e518d2d9050752bb99d77356936
sziroks <smoczynskioskar@gmail.com>
2024-04-28 13:30:17 +0200

    Added specialized classes for views

--- forms/reviews/urls.py
+++ forms/reviews/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.ReviewView.as_view(), name="review"),
-    path("thank-you", views.thank_your, name="thank-you"),
+    path("", views.ReviewSubmitView.as_view(), name="review-form"),
+    path("thank-you", views.ThankYouView.as_view(), name="thank-you"),
+    path("reviews", views.ReviewView.as_view(), name="reviews"),
+    path("reviews/<int:pk>", views.ReviewDetailView.as_view(), name="review-detail"),
 ]
--- forms/reviews/views.py
+++ forms/reviews/views.py
@@ -1,33 +1,46 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.views import View
+from django.views.generic.base import TemplateView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 from .forms import ReviewForm
 from .models import Review
-from django.views import View
 
 
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForm()
-        return render(
-            request,
-            "reviews/review.html",
-            {"form": form},
-        )
-
-    def post(self, request):
-        form = ReviewForm(request.POST)
-
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("/thank-you")
-
-        form = ReviewForm()
-        return render(
-            request,
-            "reviews/review.html",
-            {"form": form},
-        )
+class ReviewSubmitView(CreateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
+    success_url = "/thank-you"
 
 
-def thank_your(request):
-    return render(request, "reviews/thank_you.html")
+
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you.html"
+
+
+class ReviewView(ListView):
+    template_name = "reviews/review_list.html"
+    model = Review
+    context_object_name = "reviews"
+
+
+class ReviewDetailView(DetailView):
+    template_name = "reviews/review_detail.html"
+    model = Review
+
+
+    # def post(self, request):
+    #     form = ReviewForm(request.POST)
+
+    #     if form.is_valid():
+    #         form.save()
+    #         return HttpResponseRedirect("/thank-you")
+
+    #     form = ReviewForm()
+    #     return render(
+    #         request,
+    #         "reviews/review.html",
+    #         {"form": form},
+    #     )
