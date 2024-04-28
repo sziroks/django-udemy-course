426e5daee09b9ee6d4fa655c7725ffade83870b7
sziroks <smoczynskioskar@gmail.com>
2024-04-28 19:21:12 +0200

    Removed unnecessary method

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -8,21 +8,18 @@ from django.views.generic import ListView, DetailView
 from .models import Post
 
 
-def get_date(post):
-    return post["date"]
-
-
 class StartingPageView(View):
     def get(self, request):
         latest_posts = Post.objects.order_by("-date")[:3]
         return render(request, "blog/index.html", {"posts": latest_posts})
-    
+
+
 class AllPostsView(ListView):
     template_name = "blog/all-posts.html"
     model = Post
     context_object_name = "all_posts"
 
+
 class PostDetailView(DetailView):
     template_name = "blog/post-detail.html"
     model = Post
-    
