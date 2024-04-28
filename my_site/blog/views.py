8ee1df88d4154ffbcb3449e1397d8b986ba1e8db
sziroks <smoczynskioskar@gmail.com>
2024-04-28 19:18:50 +0200

    Reworked views to class based

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from datetime import date
+from django.views import View
+from django.views.generic import ListView, DetailView
 
 from .models import Post
 
@@ -10,16 +12,17 @@ def get_date(post):
     return post["date"]
 
 
-def starting_page(request):
-    latest_posts = Post.objects.order_by("-date")[:3]
-    return render(request, "blog/index.html", {"posts": latest_posts})
+class StartingPageView(View):
+    def get(self, request):
+        latest_posts = Post.objects.order_by("-date")[:3]
+        return render(request, "blog/index.html", {"posts": latest_posts})
+    
+class AllPostsView(ListView):
+    template_name = "blog/all-posts.html"
+    model = Post
+    context_object_name = "all_posts"
 
-
-def posts(request):
-    all_posts = Post.objects.all()
-    return render(request, "blog/all-posts.html", {"all_posts": all_posts})
-
-
-def post_detail(request, slug):
-    post = get_object_or_404(Post, slug=slug)
-    return render(request, "blog/post-detail.html", {"post": post})
+class PostDetailView(DetailView):
+    template_name = "blog/post-detail.html"
+    model = Post
+    
